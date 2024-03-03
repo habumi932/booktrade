3cb7d707b5fc2968562fbc49853ee502110f9b9a
Hang Bui <hangbm2002@gmail.com>
2024-03-03 18:54:56 -0500

    Add check if user is still logged in in LogoutActivity.java

--- app/src/main/java/com/hangbui/booktrade/LogoutActivity.java
+++ app/src/main/java/com/hangbui/booktrade/LogoutActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.hangbui.booktrade.databinding.ActivityLogoutBinding;
 
 public class LogoutActivity extends AppCompatActivity {
@@ -15,8 +17,16 @@ public class LogoutActivity extends AppCompatActivity {
     private View.OnClickListener button_login_clickListener = new View.OnClickListener() {
 
         public void onClick(View v) {
-            Intent theIntent = new Intent(LogoutActivity.this, LoginActivity.class);
-            startActivity(theIntent);
+            // If user is logged in then go to home page, if not go to login page
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            if(user != null) {
+                Intent theIntent = new Intent(LogoutActivity.this, HomeActivity.class);
+                startActivity(theIntent);
+            }
+            else {
+                Intent theIntent = new Intent(LogoutActivity.this, LoginActivity.class);
+                startActivity(theIntent);
+            }
         }
     };
     private View.OnClickListener button_register_clickListener = new View.OnClickListener() {
