d4a19334b3856b2ded988e7b8c0adb3e7beb5426
Hang Bui <hangbm2002@gmail.com>
2024-03-04 17:10:10 -0500

    Add functionality to bottom navigation bar in home page

--- app/src/main/java/com/hangbui/booktrade/HomeActivity.java
+++ app/src/main/java/com/hangbui/booktrade/HomeActivity.java
@@ -1,8 +1,12 @@
 package com.hangbui.booktrade;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.provider.Settings;
 
 import com.hangbui.booktrade.databinding.ActivityHomeBinding;
 
@@ -15,6 +19,28 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
 
+        binding.bottomNavigationBar.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if(itemId == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if(itemId == R.id.profile) {
+                replaceFragment(new ProfileFragment());
+            } else if(itemId == R.id.books) {
+                replaceFragment(new BooksFragment());
+            } else if(itemId == R.id.settings) {
+                replaceFragment(new SettingsFragment());
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
