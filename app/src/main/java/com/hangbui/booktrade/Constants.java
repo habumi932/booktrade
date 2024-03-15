fd037714eb0e62e6758f7ab618cd2f38488fa2c8
Hang Bui <hangbm2002@gmail.com>
2024-03-15 14:46:50 -0400

    Add final to constants declaration

--- app/src/main/java/com/hangbui/booktrade/Constants.java
+++ app/src/main/java/com/hangbui/booktrade/Constants.java
@@ -2,14 +2,17 @@ package com.hangbui.booktrade;
 
 public class Constants {
     // Extras
-    public static String EXTRA_CURRENT_USER = "com.hangbui.booktrade.EXTRA_CURRENT_USER";
+    public static final String EXTRA_CURRENT_USER = "com.hangbui.booktrade.EXTRA_CURRENT_USER";
 
     // Users table
-    public static String USERS_TABLE = "users";
-    public static String USERS_TABLE_COL_ID = "id";
-    public static String USERS_TABLE_COL_EMAIL = "email";
-    public static String USERS_TABLE_COL_NAME = "name";
-    public static String USERS_TABLE_COL_PHOTO_URL = "photoUrl";
-    public static String USERS_TABLE_COL_UNIVERSITY = "university";
+    public static final String USERS_TABLE = "users";
+    public static final String USERS_TABLE_COL_ID = "id";
+    public static final String USERS_TABLE_COL_EMAIL = "email";
+    public static final String USERS_TABLE_COL_NAME = "name";
+    public static final String USERS_TABLE_COL_PHOTO_URL = "photoUrl";
+    public static final String USERS_TABLE_COL_UNIVERSITY = "university";
+
+    // Books table
+    public static final String BOOKS_TABLE = "books";
 }
 
