1c801bdb37f4061141bf81bb7b9cf9a26b12df4e
Hang Bui <hangbm2002@gmail.com>
2024-04-14 17:06:53 -0400

    Display trade requests

--- app/src/main/java/com/hangbui/booktrade/TradeRequestsFragment.java
+++ app/src/main/java/com/hangbui/booktrade/TradeRequestsFragment.java
@@ -1,5 +1,7 @@
 package com.hangbui.booktrade;
 
+import static com.hangbui.booktrade.Constants.BOOKS_TABLE;
+import static com.hangbui.booktrade.Constants.BOOKS_TABLE_COL_BOOK_ID;
 import static com.hangbui.booktrade.Constants.BOOK_REQUESTS_TABLE;
 import static com.hangbui.booktrade.Constants.BOOK_REQUESTS_TABLE_COL_RECEIVER_ID;
 import static com.hangbui.booktrade.Constants.EXTRA_CURRENT_USER;
@@ -13,6 +15,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -21,6 +24,7 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -71,7 +75,6 @@ public class TradeRequestsFragment extends Fragment {
         }
         db = FirebaseFirestore.getInstance();
         currentUser = getActivity().getIntent().getParcelableExtra(EXTRA_CURRENT_USER);
-        getTradeRequests(currentUser.getId());
     }
 
     @Override
@@ -79,6 +82,11 @@ public class TradeRequestsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_trade_requests, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        getTradeRequests(currentUser.getId());
     }
 
     private void getTradeRequests(String uid) {
@@ -95,12 +103,44 @@ public class TradeRequestsFragment extends Fragment {
                                 requests.add(request);
                             }
                             updateTradeRequestsList(requests);
+                        } else {
+                            Log.e("TradeRequest", "Error retrieving rtrade requests");
                         }
                     }
                 });
     }
 
     private void updateTradeRequestsList(ArrayList<TradeRequest> requests) {
+        ArrayList<String> bookIds = new ArrayList<>();
+        for (TradeRequest request : requests) {
+            bookIds.add(request.getBookId());
+        }
 
+        db.collection(BOOKS_TABLE)
+                .whereIn(BOOKS_TABLE_COL_BOOK_ID, bookIds)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()) {
+                            ArrayList<Book> books = new ArrayList<>();
+                            for(QueryDocumentSnapshot document : task.getResult()) {
+                                Book theBook = document.toObject(Book.class);
+                                books.add(theBook);
+                            }
+                            updateListviewBooks(books);
+                        } else {
+                            Log.e("Book", "Error retrieving book info");
+                        }
+                    }
+                });
+    }
+
+    private void updateListviewBooks(List<Book> books) {
+        View view = getView();
+        CustomAdapterBooks adapter = new CustomAdapterBooks(getActivity(), books);
+        ListView listviewTradeRequests = view.findViewById(R.id.listview_trade_requests);
+        // listviewBooks.setOnItemClickListener(listview_trade_);
+        listviewTradeRequests.setAdapter(adapter);
     }
 }
